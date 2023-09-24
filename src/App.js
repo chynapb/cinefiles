dfff61a63fd6fd2830f91c75e892b655f4d672a9
Chyna <chynapblair@gmail.com>
2023-09-24 09:42:35 -0400

    Messing things up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Favorites } from './pages/Favorites';
+import { Login } from './pages/Login';
+import { Watchlist } from './pages/Watchlist';
 
 function App() {
   return (
-    <div className='app'>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='favorites' element={<Favorites />} />
+        <Route path='login' element={<Login />} />
+        <Route path='watchlist' element={<Watchlist />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Stack } from '@mui/material';
 
 export const Navbar = () => {
   return (
-    <AppBar position='static' style={{ background: '#1d2129' }}>
-      <Toolbar>
-        <Typography
-          variant='h5'
-          href='/'
-          sx={{
-            fontFamily: 'Outfit',
-            letterSpacing: '.1rem',
-            fontSize: '2rem',
-          }}
-        >
-          CINEFILES
-        </Typography>
-      </Toolbar>
+    <AppBar position='static' style={{ background: 'rgba(0, 0, 0, 0.8)' }}>
+      <Container maxWidth='xl'>
+        <Toolbar>
+          <Typography
+            className='header'
+            variant='h5'
+            href='/'
+            sx={{
+              flexGrow: 1,
+              fontFamily: 'Outfit',
+              letterSpacing: '.1rem',
+              fontSize: '2rem',
+            }}
+          >
+            <span>CINE</span>FILES
+          </Typography>
+          <Stack direction='row' spacing={2}>
+            <button className='link-btn'>HOME</button>
+            <button className='link-btn'>FAVORITES</button>
+            <button className='link-btn'>WATCHLIST</button>
+            <button className='link-btn'>LOGIN</button>
+          </Stack>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
