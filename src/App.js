f71368927ec56484e848d85e0a967e451a11f387
Chyna <chynapblair@gmail.com>
2023-09-26 11:17:04 -0400

    Sizing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import { Favorites } from './pages/Favorites';
 import { Watchlist } from './pages/Watchlist';
 import { Login } from './pages/Login';
 import { Footer } from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='favorites' element={<Favorites />} />
-        <Route path='login' element={<Login />} />
-        <Route path='watchlist' element={<Watchlist />} />
-      </Routes>
-      <Footer />
+      <Container className='mt-5 pt-5'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='favorites' element={<Favorites />} />
+          <Route path='login' element={<Login />} />
+          <Route path='watchlist' element={<Watchlist />} />
+        </Routes>
+        <Footer />
+      </Container>
     </>
   );
 }
