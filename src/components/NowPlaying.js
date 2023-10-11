610607339e38c0f13739b9af081e19383875655f
Chyna <chynapblair@gmail.com>
2023-10-11 06:27:16 -0400

    Carousel functionality

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -1,3 +1,91 @@
+import Carousel from 'react-grid-carousel';
+
 export const NowPlaying = () => {
-  return <h2 className='now-playing-header'>NOW PLAYING</h2>;
+  return (
+    <div className='carousel'>
+      <h2 className='now-playing-header'>NOW PLAYING</h2>
+      <Carousel
+        mobileBreakpoint={500}
+        cols={3}
+        rows={1}
+        gap={20}
+        responsiveLayout={[
+          {
+            breakpoint: 500,
+            cols: 3,
+            rows: 1,
+            gap: 10,
+            loop: true,
+            autoplay: 3000,
+          },
+        ]}
+      >
+        <Carousel.Item>
+          <img
+            width='100%'
+            src='/imgs/lady-bird.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/midsommar.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/la-la-land.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/american-psycho.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/inception.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/pearl.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/eternal-sunshine.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src='/imgs/gone-girl.jpg'
+            className='carousel-img'
+            style={{ objectFit: 'cover' }}
+            alt='Cover '
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
