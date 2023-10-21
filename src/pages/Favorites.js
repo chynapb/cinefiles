34f92a2615b44b3a2fdc67c8dca0b57c57b80e9f
Chyna <chynapblair@gmail.com>
2023-10-21 13:28:28 -0400

    Favorites and watchlist

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 export const Favorites = () => {
+  const { user } = UserAuth();
+
   return (
     <div className='favorites'>
       <h1>Your Favorites:</h1>
-      <p>
-        Please{' '}
-        <Link to='/login' className='underline'>
-          login
-        </Link>{' '}
-        to see your favorite movies.
-      </p>
+      {user ? (
+        <div>
+          <p>Movie</p>
+          <p>Movie</p>
+          <p>Movie</p>
+          <p>Movie</p>
+        </div>
+      ) : (
+        <p>
+          Please{' '}
+          <Link to='/login' className='underline'>
+            login
+          </Link>{' '}
+          to see your favorite movies.
+        </p>
+      )}
     </div>
   );
 };
--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,16 +1,38 @@
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 export const Watchlist = () => {
+  const { user } = UserAuth();
+
   return (
     <div className='watchlist'>
       <h1>Your Watchlist:</h1>
-      <p>
-        Please{' '}
-        <Link to='/login' className='underline'>
-          login
-        </Link>{' '}
-        to see your watchlist.
-      </p>
+      {user ? (
+        <>
+          <h3>To Watch</h3>
+          <div>
+            <p>Movie</p>
+            <p>Movie</p>
+            <p>Movie</p>
+            <p>Movie</p>
+          </div>
+          <h3>Watched:</h3>
+          <div>
+            <p>Movie</p>
+            <p>Movie</p>
+            <p>Movie</p>
+            <p>Movie</p>
+          </div>
+        </>
+      ) : (
+        <p>
+          Please{' '}
+          <Link to='/login' className='underline'>
+            login
+          </Link>{' '}
+          to see your watchlist.
+        </p>
+      )}
     </div>
   );
 };
