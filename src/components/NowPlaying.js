f71c126839083afb0fec56540a10246679051ceb
Chyna <chynapblair@gmail.com>
2023-11-01 07:09:58 -0400

    Swiper functionality

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -1,12 +1,13 @@
-import Carousel from 'react-grid-carousel';
 import requests from '../Api';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 export const NowPlaying = () => {
   const [movies, setMovies] = useState([]);
-
-  const movie = movies[Math.floor(Math.random() * movies.length)];
+  const swiperElRef = useRef(null);
 
   useEffect(() => {
     axios.get(requests.nowPlaying).then((res) => {
@@ -15,33 +16,27 @@ export const NowPlaying = () => {
   }, []);
 
   return (
-    <div className='carousel'>
+    <>
       <h2 className='now-playing-header'>NOW PLAYING</h2>
-      <Carousel
-        mobileBreakpoint={768}
-        cols={3}
-        rows={1}
-        gap={20}
-        responsiveLayout={[
-          {
-            breakpoint: 768,
-            cols: 3,
-            rows: 1,
-            gap: 10,
-            loop: true,
-            autoplay: 3000,
-          },
-        ]}
-      >
-        <Carousel.Item>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
-            className='carousel-img'
-            style={{ objectFit: 'cover' }}
-            alt={movie?.title}
-          />
-        </Carousel.Item>
-      </Carousel>
-    </div>
+      <div className='swiper'>
+        <swiper-container
+          ref={swiperElRef}
+          slides-per-view='3'
+          navigation='true'
+          scrollbar
+        >
+          {movies.map((movie, id) => (
+            <swiper-slide key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+                className='swiper-img'
+                style={{ objectFit: 'cover' }}
+                alt={movie?.title}
+              />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      </div>
+    </>
   );
 };
