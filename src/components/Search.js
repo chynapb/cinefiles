276c062c535278160a42c048853cbb6d6b6452ca
Chyna <chynapblair@gmail.com>
2023-11-05 11:36:20 -0500

    Search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,52 @@
 import { Container } from 'react-bootstrap';
 import SearchIcon from '@mui/icons-material/Search';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import requests from '../Api';
+import axios from 'axios';
 
 export const Search = () => {
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (handleSearch) {
+      axios.get(requests.search + `&query=${query}`).then((res) => {
+        setSearchResults(res.data.results);
+      });
+    }
+  });
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (query.length === 0) {
+      alert('Please enter a movie title to search.');
+      return;
+    } else {
+      navigate('/search-results', {
+        state: {
+          query: query,
+          results: searchResults,
+        },
+      });
+    }
+  };
+
   return (
     <Container className='search-container'>
       <h5 className='search-header'>SEARCH MOVIES</h5>
       <div className='search'>
-        <form>
-          <input
-            type='text'
-            placeholder='Enter movie title...'
-            className='search-input'
-          />
-          <button className='search-btn'>
-            <SearchIcon />
-          </button>
-        </form>
+        <input
+          type='text'
+          placeholder='Enter movie title...'
+          className='search-input'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='search-btn' onClick={handleSearch}>
+          <SearchIcon />
+        </button>
       </div>
     </Container>
   );
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,8 +1,30 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { MovieCard } from '../components/MovieCard';
+import { Container } from 'react-bootstrap';
+
 export const SearchResults = () => {
+  const location = useLocation();
+  const movies = location.state.results;
+
   return (
     <div className='results'>
+      <div className='search'>
+        <input
+          type='text'
+          placeholder='Enter movie title...'
+          className='search-input'
+        />
+        <button className='search-btn'>
+          <SearchIcon />
+        </button>
+      </div>
       <h2 className='results-header'>SEARCH RESULTS</h2>
-      <p>Showing results for ...</p>
+      <p>Showing results for "{location.state.query}"</p>
+      <Container className='grid mt-5'>
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
+      </Container>
     </div>
   );
 };
