5d3ce672b0f5b61d8b0c3310dc03f2f6975ea0ef
Chyna <chynapblair@gmail.com>
2023-11-26 09:04:15 -0500

    Updates

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -20,6 +20,7 @@ export const Details = () => {
       )
       .then((res) => {
         setMovie(res.data);
+        console.log(res.data);
         setLoading(false);
       });
   }, [id, key]);
@@ -30,7 +31,17 @@ export const Details = () => {
         <Loading />
       ) : (
         <div className='movie-details'>
+          <img
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w400/${movie.poster_path}`
+                : 'imgs/no-image.png'
+            }
+            alt={movie.title}
+          />
           <h2>{movie.title}</h2>
+          <p>{movie.release_date}</p>
+          <div className='movie-description'>{movie.overview}</div>
         </div>
       )}
     </>
